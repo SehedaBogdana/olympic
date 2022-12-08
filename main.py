328691826a86ff41d52ac129e3e39ef12775d374
Anastasiiasyvak <asyvak@kse.org.ua>
2022-12-08 19:17:22 +0200

    add split

--- main.py
+++ main.py
@@ -2,4 +2,15 @@ import sys
 file_with_data = sys.argv[1]
 "./"  # ./ поточна папка
 country = sys.argv[3]
+
 print("country = ", country)
+
+with open(file_with_data, 'r') as file:
+    line = file.readline()
+    while line != "":
+        line = file.readline()
+        line_splitted = line.split("\t")
+        print(line_splitted)
+
+
+
