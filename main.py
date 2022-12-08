dea55903bd36ac076c2a3ba1a283e2df8fd837e4
SehedaBogdana <bseheda@kse.org.ua>
2022-12-08 20:19:53 +0300

    split

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import sys
 file_with_data = sys.argv[1]
-"./"  # ./ поточна папка
+# ./ поточна папка
 country = sys.argv[3]
+year = sys
+
 print("country = ", country)
+
+with open(file_with_data, 'r') as file:
+    line = file.readline()
+    #current_country = line[6]
+    while line != "":
+        line = file.readline()
+        line_splitted = line.split("\t")
+        print(line_splitted)
