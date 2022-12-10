c752c904eb49f190ba245512ec4f585b4088c5b3
Anastasiiasyvak <asyvak@kse.org.ua>
2022-12-10 17:05:04 +0200

    print name and sport of country

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 import sys
-file_with_data = sys.argv[1]
 # ./ поточна папка
-country = sys.argv[3]
-year = sys
-year_set = set()
-with open(file_with_data, 'r') as file:
 
-    #current_country = line[6]
-    line = file.readline()
+def the_best_ten_champions():
+    pass
+
+def total_amount_of_medals():
+    pass
+
+def extreme_cases():
+    pass
+
+
+def output():
+    pass
+
+
+data_file = sys.argv[1]
+country = sys.argv[3]
+year = sys.argv[4]
+quality = 0
+
+with open(data_file, 'r') as file:
+    file.readline()
     line = file.readline()
     while line != "":
-        line_splitted = line.split("\t")
-        year = int(line_splitted[9])
-        year_set.add(year)
+        line_split = line.split("\t")
+        current_country = line_split[6]
+        NAC = line_split[7]
+        medal = line_split[14]
+        name = line_split[1]
+        sport = line_split[12]
         line = file.readline()
-
-year_list = sorted(year_set)
-
-for i, y in enumerate(year_list[:10], 1):
-    print(i, "\t", y)
+        if country == current_country or country == NAC:
+            line = file.readline()
+            if year in line_split and country in line_split:
+                line = file.readline()
+                print(f"{name} - {sport}")
 
