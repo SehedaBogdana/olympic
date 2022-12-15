857a8f0d9292cbdc3715c91eabc69a62dfa28dbe
Anastasiiasyvak <asyvak@kse.org.ua>
2022-12-15 10:42:30 +0200

    first and forth

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ import sys
 
 data_file = sys.argv[1]
 command = sys.argv[2]
-country = sys.argv[3]
-year = sys.argv[4]
-output_t = sys.argv[6]
-quantity = 0
-all_years = []
-all_countries = []
-
 
 if command == "-medals":
+    country = sys.argv[3]
+    year = sys.argv[4]
+    output_t = sys.argv[6]
+    all_countries = []
+    quantity = 0
+    all_years = []
     with open(data_file, 'r') as file:
         file.readline()
         line = file.readline()
@@ -57,7 +56,6 @@ if command == "-medals":
             current_year = line_split[9]
             sum_of_medals = (int(gold) + int(bronze) + int(silver))
             line = file.readline()
-            all_years.append(current_year)
             if country == current_country or country == NOC:
                 if year in line_split and current_year == year:
                     if medal == "Gold\n":
@@ -72,5 +70,43 @@ if command == "-medals":
                 print("This country doesn't have more than 10 medals")
             else:
                 print(f"Gold medals - {gold}, Silver medals - {silver}, Bronze medals - {bronze}")
+
+elif command == "-interactive":
+    medals = {}
+    place = {}
+    our_list = []
+    all_countries = []
+    each_olymp = {
+        "Gold": 0,
+        "Bronze": 0,
+        "Silver": 0
+    }
+    country = input("Enter the country: ")
+    with open(data_file, 'r') as file:
+        file.readline()
+        line = file.readline()
+        while line != "":
+            line_split = line.split("\t")
+            current_country = line_split[6]
+            NOC = line_split[7]
+            current_medal = line_split[14]
+            current_place = line_split[11]
+            current_years = line_split[9]
+            all_countries.append(current_country)
+            line = file.readline()
+            if NOC == country or current_country == country:
+                if current_medal != "NA\n":
+                    if current_years in medals:
+                        medals[current_years] = medals[current_years] + 1
+                    else:
+                        medals[current_years] = 1
+                        our_list.append(current_years)
+                        place[current_place] = current_years
+        else:
+            print(f"{min(place, key=place.get)} where was their first game")
+            print(f"{min(our_list)} - is the first year when the country participated")
+            print(f"{max(medals, key=medals.get)} - the best year")
+            print(f"{min(medals, key=medals.get)} - the worst year")
+
 
 
